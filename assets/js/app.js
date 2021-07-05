427f061ee3cf0300afebd9673657d543bd9e5dd3
Carlose <carlosviniwork@gmail.comm>
2021-07-04 21:03:23 -0300

    Script para mudar a mensagem

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,22 @@
 // Aparecer o biscoito quebrado
+const msg = document.querySelector('.msg')
 
 const biscoito = document.querySelector('#bis-love');
 const container = document.querySelector('.container')
 
+
+let mensagem = ['seu corno', 'voce é broxa', 'hoje nãooooo', 'bom dia', 'boa tarde', 'javascript', 'python', 'sua mãe usa calcinha', 'seu pai é meu' ]
+
+
+
 biscoito.addEventListener('click', function() {
     container.classList.toggle('show')
+    msg.innerHTML = mensagem[Math.floor(Math.random() * mensagem.length)];
 })
+
+
+
+
+
+
+
